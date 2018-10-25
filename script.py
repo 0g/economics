e6d3dc183dcb69f90a8e08357ba5fa52506bc067
øg <pjz9@wildcats.unh.edu>
2018-10-24 22:28:03 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,59 @@
+# introduction
+print('\nWelcome to the Maximum Revenue Finder by Patrick Cash')
+print('This was developed to solve a tricky bonus Econ question\n')
+
+# declare some variables...
+factors = {}
+values = []
+intercept = float(input('Please insert the intercept: '))
+price_coefficient = float(input('Please provide the price coefficient: '))
+
+# add the intercept to the values list...
+values.append(intercept)
+
+# add on as many factors as needed...
+
+while True:
+    a = input('Please enter another coefficient, if none type "none": ')
+    if a == 'none':
+        break
+    b = input('Please enter the value for this coefficient: ')
+
+    factors[float(a)] = float(b)
+
+# solve for all factors except price...
+for k, v in factors.items():
+    factor = k * v
+    values.append(float(factor))
+
+# add all factors together...
+total = sum(values)
+
+# move constant to otherside of equation...
+if total < 0:
+    new_total = abs(total)
+else:
+    new_total = total * -1
+
+# divide by price coefficient to isolate the P variable...
+vertical_intercept = new_total / price_coefficient
+horizontal_intercept = total
+print('\nVertical Intercept: ' + str(vertical_intercept))
+print('Horizontal Intercept: ' + str(horizontal_intercept) + '\n')
+
+# find price that will provide max revenue...
+minimum = .00001
+total_rev = 0
+last_price = 0
+while True:
+    price_level = minimum * price_coefficient
+    fixed_total = total + price_level
+    total_rev = minimum * fixed_total
+    if total_rev < last_price:
+        best_price = minimum - .00001
+        break
+    else:
+        last_price = total_rev
+        minimum += .00001
+
+print('Best Price: ' + str(best_price) + '\n')
